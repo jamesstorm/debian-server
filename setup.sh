aed5add3a000252f42b116f2cc8b2f95a0866a25
James Storm <jamesstorm@gmail.com>
2023-01-03 16:08:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,17 +3,12 @@
 echo "Hello, Debain Server"
 
 # need to run this first manually
-# cd ~ && apt-get update && apt-get install git curl sudo -y && apt-get upgrade -y && git clone https://github.com/jamesstorm/debian-server && cd ~/debian-server && chmod +x setup.sh && ./setup.sh
+# cd ~ && apt-get update && apt-get install git -y && apt-get upgrade -y && git clone https://github.com/jamesstorm/debian-server && cd ~/debian-server && chmod +x setup.sh && ./setup.sh
 
 # apt update upgrade
 apt-get update 
-apt install apt-transport-https ca-certificates curl software-properties-common -y
+apt install apt-transport-https ca-certificates curl software-properties-common sudo curl -y
 apt-get upgrade -y
-
-#create a user then collect a passwd
-#adduser --disabled-password --gecos "" james
-#echo "james ALL=PASSWD: ALL" > /etc/ers.d/james
-#passwd
 
 #Github CLI
 echo "Github CLI"
