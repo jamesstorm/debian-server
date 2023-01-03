8a3f18e720c25969791779809f96452d88409c37
James Storm <jamesstorm@gmail.com>
2023-01-03 17:30:38 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,6 +15,18 @@ adduser --disabled-password --gecos "" james
 echo "james ALL=PASSWD: ALL" > /etc/ers.d/james
 passwd
 
+
+#Github CLI
+echo "Github CLI"
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& apt update \
+&& apt install gh -y
+
+gh auth login
+
+
 cd /home/james/
 
 gh repo clone jamesstorm/prime
