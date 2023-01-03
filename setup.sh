6cea08d4196ff8817be1f98f4a513d54996519d8
James Storm <jamesstorm@gmail.com>
2023-01-03 14:47:31 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,15 +12,14 @@ apt-get upgrade -y
 
 #create a user then collect a passwd
 #adduser --disabled-password --gecos "" james
-#echo "james ALL=PASSWD: ALL" > /etc/sudoers.d/james
+#echo "james ALL=PASSWD: ALL" > /etc/ers.d/james
 #passwd
 
 #Github CLI
 echo "Github CLI"
-type -p curl >/dev/null || sudo apt install curl -y
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && apt update \
 && apt install gh -y
 
