8c5b13dac6f7f7a70390f6b9395db7c3e6215df8
James Storm <jamesstorm@gmail.com>
2023-01-03 14:43:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,16 +19,18 @@ apt-get upgrade -y
 echo "Github CLI"
 type -p curl >/dev/null || sudo apt install curl -y
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+&& apt update \
+&& apt install gh -y
 
 gh auth login
 
+cd ~
+
 gh repo clone jamesstorm/prime
 
-cd ../prime/debian
+cd ~/prime/debian
 
 chmod +x setup.sh
 ./setup.sh
