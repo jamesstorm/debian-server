7fca5306ecd8a367fddd9794acf167a78d912413
James Storm <jamesstorm@gmail.com>
2023-01-03 17:23:52 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,24 +10,21 @@ apt-get update
 apt install apt-transport-https ca-certificates software-properties-common sudo curl wget -y
 apt-get upgrade -y
 
-#Github CLI
-echo "Github CLI"
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& apt update \
-&& apt install gh -y
+#create a user then collect a passwd
+adduser --disabled-password --gecos "" james
+echo "james ALL=PASSWD: ALL" > /etc/ers.d/james
+passwd
 
-gh auth login
-
-cd ~
+cd /home/james/
 
 gh repo clone jamesstorm/prime
 
-cd ~/prime/debian
+cd home/james/prime/debian
 
 chmod +x setup.sh
-./setup.sh
+
+echo "Now login as james and run /home/james/prime/debian/setup.sh"
+
 
 
 
