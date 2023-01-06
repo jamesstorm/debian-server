cde1c4a5ddd54a9159a39f25838825d5be075aee
James Storm <jamesstorm@gmail.com>
2023-01-06 21:28:06 +0000

    some tweaks and comments

--- setup.sh
+++ setup.sh
@@ -3,17 +3,15 @@
 # at a terminal
 # cd ~ && sudo apt-get update && sudo apt-get install git -y && sudo apt-get upgrade -y && git clone https://github.com/jamesstorm/debian-server && cd /home/james/debian-server && chmod +x setup.sh && ./setup.sh
 
+USERNAME="james"
 
+# This script should be running as james
 
-# this script should be running as james
-
-if [$user != james]
+if [ "$USER" != "$USERNAME" ]
 then
-    echo "Run this script as james"
+    echo "Run this script as $USERNAME"
     exit 0
 fi
-
-echo "HERE WE GO!"
 
 cd ~
 
@@ -53,7 +51,8 @@ gh repo clone jamesstorm/prime /home/james/prime
 # need to to do this for when we need to sudo calls to AWS CLI (not that we do that much any more)
 sudo cp -R /hame/james/.aws /root/
 
-#neovim - Nightly
+# Neovim - Nightly, baby!
+#
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.deb .
 sudo dpkg -i --force-overwrite ./nvim-linux64.deb
 rm nvim-linux64.deb
@@ -61,14 +60,19 @@ rm nvim-linux64.deb
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+# ripgrep 
+#
+# Needed by The Primagen's reccos for Neovim setup that I loosley followed to create mine.
+# YouTube - https://www.youtube.com/watch?v=w7i4amO_zaE
+# GitHub - https://www.youtube.com/watch?v=w7i4amO_zaE
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
 sudo dpkg -i ripgrep_13.0.0_amd64.deb
 rm ripgrep_13.0.0_amd64.deb
 
-
+# Grab my Neovim config and slam it in there.
 gh repo clone jamesstorm/nvim /home/james/.config/nvim
 
-#docker
+# Docker
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
@@ -77,21 +81,24 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-#Dry
+# Dry 
+# A terminal app for reviewing state of running containers
+# 
 curl -sSf https://moncho.github.io/dry/dryup.sh | sudo sh
 sudo chmod 755 /usr/local/bin/dryup
 
 
-#ZSH
+# ZSH
+# 
 echo "ZSH"
 sudo apt install zsh -y
 chsh -s $(which zsh)
 
-#oh-my-zsh
+# ZSH - oh-my-zsh
 echo "on-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#Powerlevel10k ZSH Theme
+# Powerlevel10k ZSH Theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
